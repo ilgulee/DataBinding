cf088c0b1c60c5bd98d2450664a428b6ab4ebd67
Ilgu Lee <iglee2000ca@gmail.com>
2019-02-20 12:13:46 -0500

    Set project to use dataBinding

--- app/src/main/java/ilgulee/com/databinding/MainActivity.kt
+++ app/src/main/java/ilgulee/com/databinding/MainActivity.kt
@@ -2,11 +2,16 @@ package ilgulee.com.databinding
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import ilgulee.com.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        //build project again
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
